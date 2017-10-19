eba2de179e746687682b9115a04cd808d5eed701
Michael Jang <michaeljang86@gmail.com>
2017-10-19 16:42:11 -0400

    Items displayed from array

--- src/App.js
+++ src/App.js
@@ -3,19 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+
+displayItems = (item) => (<li>{item}</li>);
+
+    render() {
+        var items = ['one', 'two', 'three'];
+        return (
+            <div>
+                <input/>
+                <button></button>
+                <ul>
+                    {items.map(this.displayItems)}
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default App;
